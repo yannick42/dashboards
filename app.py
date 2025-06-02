f56d4383946a26b7bc2db40fcba1745a3dcb70a9
Yannick <yannick4283@hotmail.com>
2025-06-02 22:17:59 +0200

    add scipy + modify main page + add Procfile

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import plotly.express as px
 import pandas as pd
 import uuid
 import kagglehub
+from scipy.stats import shapiro
 
 # Download latest version
 path = kagglehub.dataset_download("hellbuoy/car-price-prediction")
@@ -15,20 +16,55 @@ def serve_layout():
     session_id = str(uuid.uuid4())
     return html.Div([
         html.H1(children='Multiple Linear Regression', style={'textAlign':'center'}),
-        dcc.Dropdown(['gas', 'diesel'], 'gas', id='dropdown-selection'),
+        dcc.Dropdown(df.columns, 'CarName', id='dropdown-selection'),
+        dcc.Dropdown(df.columns, 'price', id='dropdown-selection-2'),
         dcc.Graph(id='graph-content'),
-        
+        html.H2(children='Normality test', style={'textAlign':'left'}),
+        dcc.Dropdown(df.select_dtypes(exclude=['object']).columns, 'price', id='dropdown-selection-for-normality'),
+        dcc.Markdown(
+            children='''
+            ### Shapiro-Wilk's test of normality
+
+            It's a statistical test proposed in ***<green>1965</green>***.
+
+            $W=\dfrac{(\sum\limits_{i=1}^n a_i x_{(i)})^2}{\sum\limits_{i=1}^n (x_i - \overline{x})^2}$
+
+            see [Wikipedia](https://en.wikipedia.org/wiki/Shapiro%E2%80%93Wilk_test) for more details
+            ''',
+            mathjax=True,
+            dangerously_allow_html=True, # to make <u></u> work ?
+            link_target="_blank"
+        ),
+        html.Br(),
+        html.Div(children=[
+            html.Span(id='shapiro-wilk-stat', style={'color': 'red', 'fontWeight': 'bold'}),
+            html.Span(', '),
+            html.Span(id='shapiro-wilk-p-value', style={'color': 'red', 'fontWeight': 'bold'}),
+        ]),
     ])
 
 app.layout = serve_layout()
 
+
 @callback(
     Output('graph-content', 'figure'),
-    Input('dropdown-selection', 'value')
+    Input('dropdown-selection', 'value'),
+    Input('dropdown-selection-2', 'value')
 )
-def update_graph(value):
-    dff = df[df.fueltype==value]
-    return px.line(dff, x='CarName', y='price')
+def update_graph(value, value2):
+    dff = df.sort_values(by=['price'])
+    return px.scatter(dff, x=value, y=value2)
+
+@callback(
+    Output('shapiro-wilk-stat', 'children'),
+    Output('shapiro-wilk-p-value', 'children'),
+    Input('dropdown-selection-for-normality', 'value')
+)
+def update_shapiro_wilk(value):
+    sample = df[[value]]
+    statistics, p_value = shapiro(sample)
+
+    return f"W={statistics:.3f}", f"p-value={p_value:.4f}"
 
 if __name__ == '__main__':
     app.run(debug=True)
